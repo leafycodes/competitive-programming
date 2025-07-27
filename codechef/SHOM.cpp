0ae51e4becb2990983cad6545bcbf1a30318e74c
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-27 13:37:55 +0530

    Missing Shoes

---
+++ codechef/SHOM.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int l, r;
+    cin >> l >> r;
+    int res = abs(l - r);
+    cout << res;
+    return 0;
+}

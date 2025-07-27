c33dc21464371ec8a7cc0d5adc9db978ce460b85
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-27 13:24:28 +0530

    bw157

---
+++ leetcode/biweekly/158/3572.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int maxSumDistinctTriplet(int[] x, int[] y) {
+        Map<Integer, Integer> xAndMaxOfY = new HashMap<>();
+
+        for (int i = 0; i < x.length; i++) {
+            int currX = x[i];
+            int currY = y[i];
+
+            if (!xAndMaxOfY.containsKey(currX) || currY > xAndMaxOfY.get(currX)) {
+                xAndMaxOfY.put(currX, currY);
+            }
+        }
+
+        if (xAndMaxOfY.size() < 3) {
+            return -1;
+        }
+
+        List<Integer> maxFromY = new ArrayList<>(xAndMaxOfY.values());
+        Collections.sort(maxFromY, Collections.reverseOrder());
+        return maxFromY.get(0) + maxFromY.get(1) + maxFromY.get(2);
+    }
+}

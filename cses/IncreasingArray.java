a77b99710e115ef682f1bb9bf61c1b1ae6882700
Anish Kumar Barik <barikanish0@gmail.com>
2025-08-01 15:01:09 +0530

    increasing array moves

---
+++ cses/IncreasingArray.java
@@ -0,0 +1,32 @@
+package cses;
+
+import java.util.Scanner;
+
+public class IncreasingArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        sc.close();
+
+        System.out.println(moves(nums));
+    }
+
+    public static long moves(int[] nums) {
+        long move = 0;
+        int prev = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < prev) {
+                move += prev - nums[i];
+            } else {
+                prev = nums[i];
+            }
+        }
+
+        return move;
+    }
+}
